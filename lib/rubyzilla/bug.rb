e1c3db61046f83079f4b44fce1bc506287a68db2
Tom McKay <thomasmckay@redhat.com>
2012-12-14 14:37:29 -0500

    taskmapper - rubyzilla really needs some work to match

--- lib/rubyzilla/bug.rb
+++ lib/rubyzilla/bug.rb
@@ -1,11 +1,38 @@
 module Rubyzilla
   class Bug
+    # From taskmapper-bugzilla:lib/provider/ticket.rb
+    attr_accessor :product_id
     attr_accessor :id
-    
+    attr_accessor :project_id
+    attr_accessor :component_id
+    attr_accessor :summary
+    attr_accessor :title
+    attr_accessor :version
+    attr_accessor :op_sys
+    attr_accessor :platform
+    attr_accessor :priority
+    attr_accessor :description
+    attr_accessor :alias
+    attr_accessor :qa_contact
+    attr_accessor :assignee
+    def assigned_to
+      @assignee
+    end
+    attr_accessor :requestor
+    def qa_contact
+      @requestor
+    end
+    attr_accessor :status
+    attr_accessor :target_milestone
+    attr_accessor :severity
+    attr_accessor :created_at
+    attr_accessor :updated_at
+
+=begin
     # Required create parameters
-    attr_accessor :product_id, :component_id, :summary, :version
-    attr_accessor :component # String value component for creation
-    @product # String value component for creation
+    attr_accessor :product_id, :summary, :version
+    attr_accessor :component_id
+    #@product # String value component for creation
     
     # Defaulted create parameters
     attr_accessor :op_sys, :platform, :priority, :severity
@@ -14,43 +41,54 @@ module Rubyzilla
     # Optional create parameters
     attr_accessor :alias, :assigned_to, :cc, :qa_contact, :status
     attr_accessor :target_milestone
-    
+=end
+
+    # To hold raw client data that came back from bugzilla
+    attr_accessor :client
+
     def initialize id=nil
       unless id.nil?
         result = Bugzilla.server.call("Bug.get_bugs", {:ids => [id]})
-        
-        @product_id   = result["bugs"][0]["internals"]["product_id"]
-        @product      = product
+
+        @client = result['bugs'][0]
+
+        # TODO: These all need rework
         @id           = result["bugs"][0]["id"]
-        @component_id = result["bugs"][0]["internals"]["component_id"]
+        @product_id   = result["bugs"][0]["product"]
+        @component_id = result["bugs"][0]["component"][0]
         @summary      = result["bugs"][0]["summary"]
-        @version      = result["bugs"][0]["internals"]["version"]
-        @op_sys       = result["bugs"][0]["internals"]["op_sys"]
-        @platform     = result["bugs"][0]["internals"]["rep_platform"]
-        @priority     = result["bugs"][0]["internals"]["priority"]
-        @description  = result["bugs"][0]["internals"]["short_desc"]
-        @alias        = result["bugs"][0]["alias"]
-        @qa_contact   = result["bugs"][0]["internals"]["qa_contact"]
-        @status       = result["bugs"][0]["internals"]["status_whiteboard"]
-        @target_milestone = result["bugs"][0]["internals"]["target_milestone"]
-        @severity     = result["bugs"][0]["internals"]["bug_severity"]
+        #@title
+        @version      = result["bugs"][0]["version"][0]
+        #@op_sys       = result["bugs"][0]["internals"]["op_sys"]
+        #@platform     = result["bugs"][0]["internals"]["rep_platform"]
+        #@priority     = result["bugs"][0]["internals"]["priority"]
+        #@description  = result["bugs"][0]["internals"]["short_desc"]
+        #@alias        = result["bugs"][0]["alias"]
+        #@qa_contact   = result["bugs"][0]["internals"]["qa_contact"]
+        #@assignee
+        #@requestor
+        @status       = result["bugs"][0]["status"]
+        #@target_milestone = result["bugs"][0]["internals"]["target_milestone"]
+        #@severity     = result["bugs"][0]["internals"]["bug_severity"]
+        #@created_at
+        #@updated_at
       end
       return self
     end
     
-    def product
-      Product.new(@product_id)
-    end
+    #def product
+    #  Product.new(@product_name)
+    #end
     
-    def product= _product
-      @product_id = _product.id
-      @product = _product.name
-    end
+    #def product= _product
+    #  @product_name = _product.name
+    #  #@product = _product.name
+    #end
     
     def create
       if Bugzilla.logged_in?
         parameters = {
-          :product => @product,
+          #:product => @product,
           :component => @component,
           :summary => @summary || "",
           :version => @version || "unspecified",
